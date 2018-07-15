1f36b29df2cb04db4885d1ee3a60e9a46153f6d3
Mayur Chhapra <mayurchhapra@gmail.com>
2018-07-16 02:24:16 +0530

    Lat-Lng Success

--- api/controllers/GetDataFromURLController.js
+++ api/controllers/GetDataFromURLController.js
@@ -4,17 +4,21 @@
  * @description :: Server-side actions for handling incoming requests.
  * @help        :: See https://sailsjs.com/docs/concepts/actions
  */
-
+var geocoder = require('geocoder');
 module.exports = {
     urlPage:(req, res)=>{
         var request = require("request");
         request("https://data.sfgov.org/resource/wwmu-gmzc.json", function(error, response, body) {
             let json_data =  JSON.parse(response.body);
 
-            for(let i in json_data){
-                console.log(json_data[i].fun_facts);
-                console.log("____________________________________________________");
-            }
+            geocoder.geocode('Junagadh', function( err, data){
+                console.log("Lat = ", data.results[0].geometry.location.lat);
+                console.log("Long = ", data.results[0].geometry.location.lng);
+            });
+            // for(let i in json_data){
+            //     console.log(json_data[i].fun_facts);
+            //     console.log("____________________________________________________");
+            // }
         }); 
        return res.view('urlPage');
     }
