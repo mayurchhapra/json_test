ac1617aa06d8b75a9dcc333d26c5ec3e7fb35f18
Mayur Chhapra <mayurchhapra@gmail.com>
2018-07-16 20:22:03 +0530

    Done insert

--- api/controllers/GetDataFromURLController.js
+++ api/controllers/GetDataFromURLController.js
@@ -4,21 +4,38 @@
  * @description :: Server-side actions for handling incoming requests.
  * @help        :: See https://sailsjs.com/docs/concepts/actions
  */
-var geocoder = require('geocoder');
+var NodeGeocoder = require('node-geocoder');
 module.exports = {
     urlPage:(req, res)=>{
         var request = require("request");
-        request("https://data.sfgov.org/resource/wwmu-gmzc.json", function(error, response, body) {
+        request("https://data.sfgov.org/resource/wwmu-gmzc.json", function(error, response) {
             let json_data =  JSON.parse(response.body);
-
-            geocoder.geocode('Junagadh', function( err, data){
-                console.log("Lat = ", data.results[0].geometry.location.lat);
-                console.log("Long = ", data.results[0].geometry.location.lng);
-            });
-            // for(let i in json_data){
-            //     console.log(json_data[i].fun_facts);
-            //     console.log("____________________________________________________");
-            // }
+            var options = {
+                provider: 'google',
+                // Optional depending on the providers
+                httpAdapter: 'https', // Default
+                apiKey: 'AIzaSyBJn9YoTvEO_7o8JcfO33ZT8kWMy9P_IVs ', // for Mapquest, OpenCage, Google Premier
+                formatter: null         // 'gpx', 'string', ...
+            };
+            var geocoder = NodeGeocoder(options);
+            for(let i in json_data){
+                if(json_data[i].locations !== undefined){
+                    // Using callback
+                    geocoder.geocode(json_data[i].locations, async function(err, res) {
+                        try{
+                            if(res.length > 0 && res[0] !== undefined){
+                                let obj = Object.assign(json_data[i],{'lat':res[0].latitude, 'lng':res[0].longitude});
+                                let data = await GetDataFromURL.create(obj);
+                                console.log(obj);
+                                console.log("____________________________________________________");    
+                            }
+                        }
+                        catch(err){
+                            console.log(err);
+                        }
+                    });
+                }
+            }
         }); 
        return res.view('urlPage');
     }
--- api/models/GetDataFromURL.js
+++ api/models/GetDataFromURL.js
@@ -8,7 +8,40 @@
 module.exports = {
 
   attributes: {
-
+    actor_1:{
+      type:'string'
+    },
+    actor_2:{
+      type:'string'
+    },
+    actor_3:{
+      type:'string'
+    },
+    director:{
+      type:'string'
+    },
+    locations:{
+      type:'string'
+    },
+    lat:{
+      type:'number'
+    },
+    lng:{
+      type:'number'
+    },
+    production_company:{
+      type:'string',
+    },
+    release_year:{
+      type:'string',
+    },
+    title:{
+      type:'string'
+    },
+    writer:{
+      type:'string'
+    }
+    
     //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
     //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
     //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
